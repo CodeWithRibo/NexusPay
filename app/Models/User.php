01aeb65e0ece2adde688e7314682e8108da9e7ba
Carl John Sto. Tomas <riboluna2818@gmail.com>
2026-03-07 20:28:59 +0800

    feat: Add relationships for Department and User models

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Model
@@ -16,6 +17,11 @@ class User extends Model
         'password',
         'role',
     ];
+
+    public function information(): HasOne
+    {
+        return $this->hasOne(UserInformation::class, 'user_id');
+    }
 
     protected $hidden = [
         'password'
