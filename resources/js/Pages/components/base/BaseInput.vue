ed226823c47f3d6a0c79b8adfcb2705b3762da15
Carl John Sto. Tomas <riboluna2818@gmail.com>
2026-03-10 18:58:52 +0800

    feat: Add BaseInput component with label, type, and placeholder props

---
+++ resources/js/Pages/components/base/BaseInput.vue
@@ -0,0 +1,22 @@
+<script setup>
+const model = defineModel();
+
+defineProps({
+    label: String,
+    type: {
+        type: String,
+        default: "text",
+    },
+    placeholder: String,
+});
+</script>
+
+<template>
+    <label class="label">{{ label }}</label>
+    <input
+        class="input"
+        :type="type"
+        v-model="model"
+        :placeholder="placeholder"
+    />
+</template>
