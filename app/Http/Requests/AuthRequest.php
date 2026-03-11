8d93cb4711a8da6bc28f5428d0191356ab6c3b49
Carl John Sto. Tomas <riboluna2818@gmail.com>
2026-03-11 20:36:00 +0800

    feat: adjust validation rules in AuthRequest to enforce student_id for students and make phone_number optional

--- app/Http/Requests/AuthRequest.php
+++ app/Http/Requests/AuthRequest.php
@@ -10,24 +10,14 @@ class AuthRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'student_id' => ['nullable', 'unique:users', 'regex:/^02000[0-9]{6}$/'],
+            'student_id' => ['nullable', 'required_if:role,student', 'unique:users', 'regex:/^02000[0-9]{6}$/'],
             'department_id' => ['nullable', 'unique:departments'],
             'email' => ['required', 'email', 'unique:users', 'regex:/^[a-zA-Z0-9]+(?:[._-][a-zA-Z0-9]+)*@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$/'],
-            'password' => [
-                'required',
-                'string',
-                'confirmed:repeat_password',
-                Password::min(8)
-                    ->letters()
-                    ->mixedCase()
-                    ->numbers()
-                    ->symbols()
-            ],
             'role' => ['required'],
             'first_name' => ['required'],
             'last_name' => ['required'],
             'address' => ['required'],
-            'phone_number' => ['required'],
+            'phone_number' => ['nullable'],
             'birth_date' => ['required'],
         ];
     }
@@ -35,6 +25,7 @@ class AuthRequest extends FormRequest
     {
         return [
             'student_id.regex' => 'The number must start with 02000 and contain at least 11 digits',
+            'student_id.required_if' => 'The student id field is required.'
         ];
     }
 
